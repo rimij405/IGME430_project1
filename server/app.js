91091a302ae8e121f420612efc3eb84b9601aa71
Ian Effendi <effendiian@gmail.com>
2018-10-11 11:56:16 -0400

    Application fix.

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@
 // Pull in modules using require.
 const http = require('http');
 const url = require('url');
-const query = require('querystring');
+// const query = require('querystring');
 const fs = require('fs');
 
 // Pull in handler files.
@@ -14,18 +14,24 @@ const style = fs.readFileSync(`${__dirname}/../hosted/style.css`);
 // Set the port.
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const getIndex = (req, res) => {
+const respond = (req, res, status, type, body) => {
+  res.writeHead(status, { 'Content-Type': type });
+  res.write(body);
+  res.end();
+};
 
+const getIndex = (req, res) => {
+  respond(req, res, 200, 'text/html', index);
 };
 
 const getStyle = (req, res) => {
-    
+  respond(req, res, 200, 'text/css', style);
 };
 
 // Set up the URL routes.
 const routes = {
-    '/': getIndex,
-    '/style.css': getStyle
+  '/': getIndex,
+  '/style.css': getStyle,
 };
 
 /**
@@ -34,14 +40,14 @@ const routes = {
  * @param {Response} res The response object returned by the HTTP server.
  */
 const onRequest = (req, res) => {
-    const parsedURL = url.parse(req.url);
-    const types = req.headers.accept.split(',');
-    const params = query.parse(parsedURL.query);
-    const { method } = req;
+  const parsedURL = url.parse(req.url);
+  // const types = req.headers.accept.split(',');
+  // const params = query.parse(parsedURL.query);
+  // const { method } = req;
 
-    if(routes[parsedURL.pathname]){
-        routes[parsedURL.pathname]();
-    }
+  if (routes[parsedURL.pathname]) {
+    routes[parsedURL.pathname](req, res);
+  }
 };
 
 // Set up the server.
@@ -49,4 +55,3 @@ http.createServer(onRequest).listen(port);
 
 // Development message to check to see if server started successfully.
 console.log(`Listening on 127.0.0.1: ${port}`);
-
